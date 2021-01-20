cb515e4ae652033cd3b861301b23c3c8e7a1d846
Christian Champaco <chrischampaco@gmail.com>
2021-01-20 11:47:45 -0800

    Successful edit of html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let number = max(72, 40);
-console.log(number);
 
 function max(numFirst, numSec) {
   return numFirst > numSec ? numFirst : numSec;
 }
+
+document.getElementById("jsedit").innerHTML = " Changed output is " + number;
